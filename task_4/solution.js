103160a60b131b7e2eee57d8341c776658baaa5c
Kate.brain <kmc.mkc@gmail.com>
2023-05-25 20:22:30 +0200

    1221

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,12 +2,23 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
+    let data=JSON.parse(json);
+    let products=data.products;
+    return products;}
 
+function renderProductsCards(json){
+    clearProducts();
+    let products=parseProducts(json);
+    let length=products.length;
+    for (let i = 0; i < length; i++) {
+        addProduct(products[i]);
+    }
+}
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
+
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
